e85777fa378049f800feebe54598d4967e10736c
lsy02231605-art <lsy02231605@gmail.com>
2026-02-17 05:43:12 +0900

    feat : [BOJ] 5717

---
+++ PS/BOJ/01000-10000/05001-06000/5717.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m,f;
+
+    while(true){
+        cin >> m >> f;
+        if(m == 0 && f == 0) break;
+        cout << m+f << "\n";
+        
+    }
+    return 0;
+
+}
