dce6d3b71a0edd96c37f1913098c7dd8c0646867
lsy02231605-art <lsy02231605@gmail.com>
2026-04-02 00:54:08 +0900

    feat : [BOJ] 2440

--- PS/BOJ/01000-10000/02001-03000/2440.cpp
+++ PS/BOJ/01000-10000/02001-03000/2440.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main(){
     int n;
+
     cin >> n;
 
-    for(int i=n; i>=1; i--){    // 몇줄을 출력할지
-        for(int j=1; j<=i; j++){   // 그 줄에 몇개의 별을 출력할지
+    for(int i=n; i>0; i--){
+        for(int j=i; j>0; j--){
             cout << "*";
         }
         cout << "\n";
     }
+
     return 0;
 }
